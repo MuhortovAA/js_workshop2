2f716ee2d89e2f10c1781957389c8c054f454515
MuhortovAA <xbestya@mail.ru>
2019-10-23 20:52:59 +0300

    lesson 1 add checked

---
+++ src/index.js
@@ -0,0 +1,13 @@
+'use strict';
+//checkbox
+const checkbox = document.getElementById('discount-checkbox');
+
+checkbox.addEventListener('change', function() {
+    if (this.checked) {
+        this.nextElementSibling.classList.add('checked');
+    } else {
+        this.nextElementSibling.classList.remove('checked');
+    }
+    console.log(this.checked);
+
+});
