d6d293de85c06745bb78096623c45b976916ff65
MuhortovAA <xbestya@mail.ru>
2019-11-03 19:58:30 +0300

    add cards to

--- src/index.js
+++ src/index.js
@@ -14,10 +14,34 @@ checkbox.forEach((elem) => {
 
 // корзина
 
-const btnCart = document.getElementById('cart');
-const modalCart = document.querySelector('.cart');
-
+const btnCart = document.getElementById('cart'),
+    modalCart = document.querySelector('.cart'),
+    closebtn = document.querySelector('.cart-close');
 btnCart.addEventListener('click', () => {
-    modalCart.style.display = 'block';
+    modalCart.style.display = 'flex';
+    document.body.style.overflow = 'hidden';
+});
+closebtn.addEventListener('click', () => {
+    modalCart.style.display = 'none';
+    document.body.style.overflow = '';
+});
+const cards = document.querySelectorAll('.goods .card'),
+    cartWrapper = document.querySelector('.cart-wrapper'),
+    cartEmpty = document.getElementById('cart-empty'),
+    countGoods = document.querySelector('.counter');
 
-})
+cards.forEach((card) => {
+    const btn = card.querySelector('button');
+    btn.addEventListener('click', () => {
+        const cardClone = card.cloneNode(true);
+        cartWrapper.appendChild(cardClone);
+        cartEmpty.remove();
+        showData();
+    });
+
+});
+
+function showData() {
+    const cardsCart = cartWrapper.querySelectorAll('.card');
+    countGoods.textContent = cardsCart.length;
+};
